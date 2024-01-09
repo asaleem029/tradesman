6cdc9dedce8b1747d760d4eb5199ac0bd13a935b
Ahmad Saleem <asaleem029@gmail.com>
2024-01-10 04:49:48 +0500

    change welcome message

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php include 'header.php'; ?>
 
 <div class="container">
-    <h1>Welcome to my lovely page</h1>
+
+    <?php if (isset($_SESSION['user']) && !empty($_SESSION['user'])) { ?>
+        <h3>Welcome <b><?= strtoupper($_SESSION['user']['name']) ?></b> To Tradesman Finder</h3>
+    <?php } else { ?>
+        <h3>Welcome To Tradesman Finder</h3>
+    <?php } ?>
     <img src="img/innovation.jpg" />
 </div>
 
